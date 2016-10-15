70eab25eecc86f577f427ec546616d35b277e0f0
Andy Little <kwikius@yahoo.com>
2016-10-15 16:24:27 +0100

    make the test magic neater

--- test_suite.cpp
+++ test_suite.cpp
@@ -1,18 +1,27 @@
 #include "../system/serial_port.hpp"
 
+namespace {
+
+   constexpr char test_magic[] = "$$$<<<***>>>$$$ ";
+}
+
 bool test(bool (*pfn)(), const char* name)
 {
-   serial_port::write("$$$***$$$ test \"");
+   serial_port::write(test_magic);
+   serial_port::write("test = \"");
    serial_port::write(name);
-   serial_port::write("\"{\n");
+   serial_port::write("\" {\n");
 
    bool result = pfn();
-   serial_port::write("$$$***$$$ ");
+   serial_port::write(test_magic);
+   serial_port::write("result = \"");
    if ( result){
        serial_port::write("PASS");
    }else{
        serial_port::write("FAIL");
    }
-   serial_port::write(";\n$$$***$$$ }\n");
+   serial_port::write("\"\n");
+   serial_port::write(test_magic);
+   serial_port::write("}\n\n");
    return result;
 }
