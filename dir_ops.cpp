d1d77b2ddf607483d41a1201a056690eb29d092a
Andy Little <kwikius@yahoo.com>
2016-10-15 16:23:56 +0100

    success if file exits or have to create it

--- dir_ops.cpp
+++ dir_ops.cpp
@@ -14,9 +14,9 @@ bool test_mkdir()
    FRESULT fr = f_getcwd(pwd_buf,30);
 
    if (fr == FR_OK){
-     serial_port::write("pwd is \"");
-     serial_port::write(pwd_buf);
-     serial_port::write("\"\n");
+      serial_port::write("pwd is \"");
+      serial_port::write(pwd_buf);
+      serial_port::write("\"\n");
    }else{
       serial_port::write("get cwd failed\n");
       serial_port::write(file_error_msg(fr));
@@ -24,42 +24,40 @@ bool test_mkdir()
       return false;
    }
    const char subdir_name[] = "aerflite";
-   // check for exist of dir to make
    FILINFO fno;
    fr = f_stat(subdir_name, &fno);
-   if (fr == FR_NO_FILE){
-      fr = f_mkdir(subdir_name);
-      if ( fr == FR_OK){
-         fr = f_chdir(subdir_name);
-         
+   switch (fr) {
+      case FR_NO_FILE: // ok doesnt exist create
+         fr = f_mkdir(subdir_name);       
          if (fr == FR_OK){
-            fr = f_getcwd(pwd_buf,30);
-            serial_port::write("dir change\n  pwd is \"");
-            serial_port::write(pwd_buf);
-            serial_port::write("\"\n");
-            return true;
-         }else{
-            serial_port::write("cd subdir failed\n");
+            serial_port::write("dir created OK\n");
+            break;
+         }else{ 
+            serial_port::write("make subdir failed\n");
             serial_port::write(file_error_msg(fr));
             serial_port::write("\n");
             return false;
          }
-      }else{
-         serial_port::write("mkdir subdir failed\n");
-         serial_port::write(file_error_msg(fr));
-         serial_port::write("\n");
-         return false;
-      }
-   }else {
-      if (fr == FR_OK){
+      case FR_OK:
          serial_port::write("subdir exists\"");
          serial_port::write(subdir_name);
          serial_port::write("\"\n");
-      }else{
+         break;
+      default:
          serial_port::write("stat subdir failed\n");
          serial_port::write(file_error_msg(fr));
          serial_port::write("\n");
-      }
+         return false;
+   }
+   fr = f_chdir(subdir_name);
+   if ( fr == FR_OK){
+      fr = f_getcwd(pwd_buf,30);
+      serial_port::write("pwd is now \"");
+      serial_port::write(pwd_buf);
+      serial_port::write("\"\n");
+      return true;
+   }else{
+      serial_port::write("chdir subdir failed\"");
       return false;
-    }
+   }
 }
